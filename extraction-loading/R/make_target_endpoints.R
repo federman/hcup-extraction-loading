2662ed10b08fabe40c65e732e1f3b50625632c58
rl627 <rl627@drexel.edu>
2023-01-06 14:31:23 -0500

    generalized make_target_endpoints to return additional collumns as per `keep_column` arguemnt #7

--- extraction-loading/R/make_target_endpoints.R
+++ extraction-loading/R/make_target_endpoints.R
@@ -1,13 +1,22 @@
 #' returns a data frame with target endpoints for .parquet and codebooks
 #'
-#'
+#' @param df_status: table containined elt_status, specifically we will mutate
+#' a valid path using dataset_id.
+#' @param keep_column: string of which other columns to keep other than dataset_id 
+#' and paths. 
 #'
 
-make_target_endpoints = function(df_status){
+make_target_endpoints = function(df_status, keep_column = 0){
+  keep_column = "codebook"
+  
+  col_index_keep = which(names(df_status)== keep_column)
+  
   df_status%>% 
     mutate(path_dta = glue("raw-hcup/{dataset_id}.dta"),
            path_parquet = glue("raw-hcup/{dataset_id}.parquet"),
            path_codebook = glue("raw-hcup/{dataset_id}_codebook.csv")) %>% 
-    select(dataset_id, contains("path")) %>% 
+    select(dataset_id, 
+           contains("path"),
+           ifelse(is_empty(col_index_keep),0,col_index_keep)) %>% 
     return()
 }
