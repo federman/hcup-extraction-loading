9d28d83f0e378eb382b842bb6549479aba4a4364
rl627 <rl627@drexel.edu>
2023-01-09 17:30:15 -0500

    Added table description to generate_source_yml() #8

--- extraction-loading/code/scr/yml_generator/generate_source_yml.R
+++ extraction-loading/code/scr/yml_generator/generate_source_yml.R
@@ -10,19 +10,20 @@ parse_hcup_table_name = function(table_name){
     str_to_lower() %>% 
     str_split('_') %>% 
     .[[1]] %>% 
-    set_names(c("state",'dataset','year','table'))
+    set_names(c("state",'database','year','table')) %>% 
+    c(.,c("name" = table_name %>% str_to_lower)) %>% 
+    as.list()
   return(table_info)
 }
 
 
-get_dbt_models_dir = function(table_name){
+get_dbt_models_dir = function(table_info){
   
   ## table parameter 
-  table_info = parse_hcup_table_name(table_name)
-  tier1_path = ifelse(table_info['dataset']%in%vec__state_datasets,'state','nationwide' )
-  tier2_path = table_info['dataset']
-  tier3_path = table_info['state']
-  tier4_path = glue("{table_name}.yml") %>% str_to_lower()
+  tier1_path = ifelse(table_info$database%in%vec__state_datasets,'state','nationwide' )
+  tier2_path = table_info$database
+  tier3_path = table_info$state 
+  tier4_path = glue("{table_info$name}.yml") %>% str_to_lower()
   
   ## get end path
   local_path = getwd()
@@ -40,22 +41,26 @@ get_dbt_models_dir = function(table_name){
   return(endpoint)
   }
 
-generate_source_yml <- function() {
+generate_source_yml <- function(table_name) {
 
   # Parameters
-  db_name = 'SEDD'
-  db_tags = c("State","Emergency Department","ER","SEDD")
-  db_desc  = '{{ doc("sedd_description") }}'
   db_external_loc = "D:\\git\\hcup-extraction-loading\\extraction-loading\\raw-hcup\\{name}.parquet"
   table_name = 'NY_SEDD_2017_CHGS'
-  table_desc = 'New York State SEDD 2017 CHGS'
+  
+  ## Intermediaa
+  table_info = parse_hcup_table_name(table_name)
+  table_desc = paste0('New York State SEDD 2017 CHGS. ','{{ doc("sedd_',table_info$table,'_description") }}' ) %>% 
+    noquote()
+  db_tags = c('State','ER','SEDD')
+  db_desc  = '{{ doc("sedd_description") }}'
+  
   
   # Create a list object representing the source.yml configuration
   source_yml <- list(
     version= as.integer(2),
     sources= list(
       list(
-        name= db_name,
+        name= table_info$database %>% str_to_upper(),
         tags= db_tags,
         description = db_desc,
         meta = list(
@@ -72,7 +77,7 @@ generate_source_yml <- function() {
   )
   
   # Check directory exists
-  endpoint = get_dbt_models_dir(table_name)
+  endpoint = get_dbt_models_dir(table_info)
   
   
   # Write actual yaml file
