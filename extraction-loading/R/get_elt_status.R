c935c68880cf68163a94a3a7408a24c7030c1337
rl627 <rl627@drexel.edu>
2023-02-27 14:32:37 -0500

    made get_elt_status() robust to data/load types (sas vs stata)

--- extraction-loading/R/get_elt_status.R
+++ extraction-loading/R/get_elt_status.R
@@ -3,7 +3,7 @@
 #' searches the /raw-hcup directory for these files. 
 
 get_elt_status = function(){
-
+  
   elt_files = list.files(path = "raw-hcup/", recursive = T)
   
   elt_status = tibble(raw = elt_files) %>% 
@@ -19,19 +19,40 @@ get_elt_status = function(){
                                    file_extension),
            dataset_id = str_remove(dataset_id, "_codebook")) %>% 
     pivot_wider(names_from = 'file_extension', values_from = value) %>% 
-    arrange(dataset_id)
+    mutate(sas7dat = NA) %>% 
+    ## handle sas vs stata
+    mutate(
+      load_program = case_when(
+        is.na(sas)&is.na(Do)~NA_character_,
+        !is.na(sas)&!is.na(Do)~'REDUNDANT',
+        !is.na(sas)~'.sas',
+        !is.na(Do)~'.Do',
+        TRUE  ~ "ERROR"  ),
+      loaded_data = case_when(
+        is.na(dta)&is.na(sas7dat)~NA_character_,
+        !is.na(dta)&!is.na(sas7dat)~'REDUNDANT',
+        !is.na(sas7dat)~'sas7dat',
+        !is.na(dta)~'dta',
+        TRUE  ~ "ERROR"  )) %>% 
+    arrange(dataset_id) %>% 
+    select(dataset_id, asc,
+           load_program, loaded_data, 
+           parquet, codebook) %>% 
+    arrange(codebook, parquet,
+            loaded_data,
+            load_program,
+            asc)
   
-  if (!"parquet"%in%names(elt_status)){
-    
-    elt_template = tibble(dataset_id = character(),
-                          asc = logical(),
-                          Do = logical(),
-                          dta = logical(),
-                          parquet = logical())
-    
-    elt_status = elt_status %>% 
-      left_join(elt_template)
-  }
+  # if (!"parquet"%in%names(elt_status)){
+  #   
+  #   elt_template = tibble(dataset_id = character(),
+  #                         asc = logical(),
+  #                         Do = logical(),
+  #                         dta = logical(),
+  #                         parquet = logical())
+  #   
+  #   elt_status = elt_status %>% 
+  #     left_join(elt_template)
+  # }
   return(elt_status)
 }
-  
