b98692b36f5e013cab4b618635e1597401175852
rl627 <rl627@drexel.edu>
2023-04-03 14:24:05 -0400

    abstract setup to .Rprofile

--- extraction-loading/.Rprofile
+++ extraction-loading/.Rprofile
@@ -1,1 +1,24 @@
-source("renv/activate.R")
+{# 0. renv --------------------------------------------------------------------
+  source("renv/activate.R")
+}
+
+{# 1. Setup ----
+  ## Load Dependencies
+  rm(list= ls()); options(scipen = 999, readr.show_col_types = FALSE)
+  library(rstudioapi) 
+  library(glue)         
+  library(stringr)
+  library(yaml)
+  library(readstata13)
+  library(data.table)
+  library(arrow)
+  library(tidyverse)
+  library(cli)
+  
+  ## Set directory and load helpers 
+  # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+  list.files(path = 'R/', all.files = T,recursive = T, full.names = T, pattern = '.R') %>% 
+    walk(~source(.x))
+  
+  cli_alert_success("HCUP ETL Setup Success!")
+}
--- extraction-loading/index.R
+++ extraction-loading/index.R
@@ -1,36 +1,16 @@
-{# 0. Setup ----
-  ## Load Dependencies
-  rm(list= ls()); options(scipen = 999, readr.show_col_types = FALSE)
-  library(rstudioapi) 
-  library(glue)         
-  library(stringr)
-  library(yaml)
-  library(readstata13)
-  library(data.table)
-  library(arrow)
-  library(tidyverse)
-  
-  ## Set directory and load helpers 
-  setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-  list.files(path = 'R/', all.files = T,recursive = T, full.names = T, pattern = '.R') %>% 
-    walk(~source(.x))
-  
-  message("Setup Success!")
-}
-
-
-
 { # 1. Extraction (done on desktop with high RAM) ----
   
   ## Step 1: Evaluate ELT status
-  get_elt_status()
-  get_elt_status() %>% filter(is.na(load_program ))
+  get_elt_status()  
   
   ## Step 2: Copy load programs
-  copy_load_program()
+  copy_load_program()  
   
   ## Step 3: Run STATA to generate dta
-  get_elt_status() %>% select(-parquet, -codebook)
+  get_elt_status() %>%
+    filter(is.na(loaded_data)) %>% 
+    arrange(load_program) %>% 
+    select(-parquet, -codebook)
   
   ## Step 4: Load as parquet
   load_dta_to_db()
