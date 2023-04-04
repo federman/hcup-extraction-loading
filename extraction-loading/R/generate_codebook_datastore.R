413b3d83df10c8dcffb6214d772a2e01765243ec
rl627 <rl627@drexel.edu>
2023-04-04 16:50:12 -0400

    added soruce summary generator to generate_codebook_datastore()

--- extraction-loading/R/generate_codebook_datastore.R
+++ extraction-loading/R/generate_codebook_datastore.R
@@ -40,27 +40,47 @@ make_codebooks = function(df_id){
 
 generate_codebooks = function(){
   
-  ## Individual codebooks 
-  df_targets_without_codebooks = get_elt_status() %>% 
-    make_target_endpoints(keep_column = 'codebook') %>% 
-    select(-path_dta) %>% 
-    filter(is.na(codebook))
-  
-  if (nrow(df_targets_without_codebooks) == 0){
-    cli_alert_success("No codebooks missing - No Action taken!")
-  } else {
-    df_targets_without_codebooks %>% 
-      group_by(row_number()) %>% 
-      group_walk(~make_codebooks(.x))
-    cli_alert_success("{nrow(df_targets_without_codebooks)} codebooks written!")
+  { # Generate individual codebooks -------------------------------------------
+    df_targets_without_codebooks = get_elt_status() %>% 
+      make_target_endpoints(keep_column = 'codebook') %>% 
+      select(-path_dta) %>% 
+      filter(is.na(codebook))
+    
+    if (nrow(df_targets_without_codebooks) == 0){
+      cli_alert_success("No codebooks missing - No Action taken!")
+    } else {
+      df_targets_without_codebooks %>% 
+        group_by(row_number()) %>% 
+        group_walk(~make_codebooks(.x))
+      cli_alert_success("{nrow(df_targets_without_codebooks)} codebooks written")
+    }
   }
   
-  ## Compiled codebook
+  { # Compile codebook --------------------------------------------------------
+    df_codebooks = get_elt_status() %>% 
+      make_target_endpoints() %>% 
+      pull(path_codebook) %>% 
+      map_df(~fread(.)) %>% 
+      as_tibble() 
+    df_codebooks%>% fwrite("clean/df_codebooks.csv")
+    cli_alert_success("Compiled codebooks at clean/df_codebooks.csv")
+  }
   
-  get_elt_status() %>% 
-    make_target_endpoints() %>% 
-    pull(path_codebook) %>% 
-    map_df(~fread(.)) %>% 
-    as_tibble() %>% 
-    fwrite("clean/df_codebooks.csv")
+  { # Compile source metadata --------------------------------------------------------
+    
+    df_summary = df_codebooks %>% 
+      count(dataset_id, name = 'n_columns') %>% 
+      rowwise() %>% 
+      mutate(split = str_split(dataset_id, "_"),
+             state = split[[1]],
+             db = split[[2]],
+             year = str_sub(split[[3]],1,4),
+             file = split[4:length(split)] %>% paste(collapse = '_')) %>% 
+      select(-split)
+    
+    df_summary %>% write_parquet("clean/df_summary.parquet")
+    cli_alert_success("Compiled source summary at clean/df_summary.parquet")
+    
+  }
+  
 }
