62dc378a4f6e979ee3bdb29d55750ce400f4335f
Ran Li <45013044+ran-codes@users.noreply.github.com>
2023-07-11 10:20:19 -0400

    update get_etl_status for uhc workstation

--- extraction-loading/R/get_etl_status.R
+++ extraction-loading/R/get_etl_status.R
@@ -1,21 +1,29 @@
-#' This function will return the status of various files available for
-#' each dataset_id. Currently it only works in the `dev` context and
-#' searches the /raw-hcup directory for these files.
+#' There are a few files we need to take stock of
+#'   - raw data: .acs 
+#'   - load program in the same directory as data: .Do or .Sas
+#'   - loaded results: .dta or .sas7bdat
+#'   - extract codebook: .csv
+#'   - extract data: .parquet
 
 get_etl_status = function() {
   
-  etl_files = list.files(path = "raw-hcup/", recursive = T) %>%
-    discard( ~ str_detect(.x, "xwalk|indicator"))
+  { ### Inventory
+    path_raw_hcup = '//files.drexel.edu/encrypted/SOPH/UHC/SchnakeMahl_HCUP/raw'
+    etl_files = list.files(path_raw_hcup, 
+                                recursive = T)  |>  
+      basename()
+  }
+
   
-  status_raw = tibble(raw = etl_files) %>%
-    rowwise() %>%
+  status_raw = tibble(raw = etl_files) |>
+    rowwise() |>
     mutate(
       raw_split = str_split(raw, "\\."),
       dataset_id = raw_split[[1]],
       file_extension = raw_split[[2]]
-    ) %>%
-    select(-contains('raw')) %>%
-    mutate(value = T) %>%
+    ) |>
+    select(-contains('raw')) |>
+    mutate(value = T) |>
     ## handle codebooks
     mutate(
       file_extension = ifelse(
@@ -24,19 +32,17 @@ get_etl_status = function() {
         file_extension
       ),
       dataset_id = str_remove(dataset_id, "_codebook")
-    ) %>%
+    ) |>
     pivot_wider(names_from = 'file_extension', values_from = value)
   
-  if (!"parquet" %in% names(status_raw)) {
-    status_raw = status_raw %>%
-      mutate(parquet = NA) }
+  if (!"Do" %in% names(status_raw))  status_raw = status_raw |> mutate(Do = NA) 
+  if (!"sas" %in% names(status_raw)) status_raw = status_raw |> mutate(sas = NA) 
+  if (!"dta" %in% names(status_raw))  status_raw = status_raw |> mutate(dta = NA) 
+  if (!"sas7bdat" %in% names(status_raw)) status_raw = status_raw |> mutate(sas7bdat = NA) 
+  if (!"parquet" %in% names(status_raw)) status_raw = status_raw |> mutate(parquet = NA) 
+  if (!"codebook" %in% names(status_raw))  status_raw = status_raw |>mutate(codebook = NA) 
   
-  if (!"Do" %in% names(status_raw)) {
-    status_raw = status_raw %>%
-      mutate(Do = NA) }
- 
-  
-  etl_status =   status_raw %>% 
+  etl_status =   status_raw |> 
     mutate(
       load_program = case_when(
         is.na(sas) & is.na(Do) ~ NA_character_,
@@ -55,11 +61,11 @@ get_etl_status = function() {
           'dta',
         TRUE  ~ "ERROR"
       )
-    ) %>%
-    arrange(dataset_id) %>%
+    ) |>
+    arrange(dataset_id) |>
     select(dataset_id, asc,
            load_program, loaded_data,
-           parquet, codebook) %>%
+           parquet, codebook) |>
     arrange(codebook, parquet,
             loaded_data,
             load_program,
